4186891748ab01fbfcd13a74edfa01fa6af37371
Joe Mock <jmock@zagmail.gonzaga.edu>
2025-04-23 16:44:19 -0700

    Added dealer class

--- src/main/java/edu/gonzaga/Users/Dealer.java
+++ src/main/java/edu/gonzaga/Users/Dealer.java
@@ -1,7 +1,48 @@
 package edu.gonzaga.Users;
 
+import edu.gonzaga.Cards.Deck;
+import edu.gonzaga.Cards.Hand;
+
 public class Dealer {
+    private String name = "Dealer";
+    private Hand dealerhand;
+
+
+    public String getName(){
+        return name;
+    }
     
-    int handValue;
+    private void setName(String name){
+        this.name = name;
+    }
+
+    public void initializeDeal(Deck deck){
+        dealerhand.popDeck(deck);
+        dealerhand.popDeck(deck);
+
+    }
+
+    // Should call hit at the end of rounds, dealer will keep hitting
+    // until their hand has a value greater than or equal to 17
+    public void hit(Deck deck){
+        while(dealerhand.getScore() < 17 || !dealerhand.bust()){
+            dealerhand.popDeck(deck);
+        }
+    }
+    // Or get hand, will only show what is in the hand at the current time
+    // We will use the second card in the hand list to mimic the game but never
+    // Ouput / reveal it
+    public void getHand(){
+        for(int i = 0; i < dealerhand.getSize(); ++i){
+            if(i != 1){
+                System.out.println(dealerhand.getCard(i));
+            }
+        }
+        System.out.println(dealerhand.getCard(0));
+        System.out.println(dealerhand.getCard(1));
+    }
+    public int getScore(){
+        return dealerhand.getScore();
+    }
 
 }
