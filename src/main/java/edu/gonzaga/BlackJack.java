299d5ee26f121bd5a2a424d7e171f3473dc7e0fa
KyleChoate <kyle7choate@gmail.com>
2025-04-28 21:48:20 -0700

    Fixed round numbering

--- src/main/java/edu/gonzaga/BlackJack.java
+++ src/main/java/edu/gonzaga/BlackJack.java
@@ -60,7 +60,7 @@ public class BlackJack
 
     public void play()
     {
-        for (int index = 0 ; index < 4 ; index++)
+        for (int index = 1 ; index < 5 ; index++)
         {
             Round curRound = new Round(party, index, deck);
             curRound.play();
